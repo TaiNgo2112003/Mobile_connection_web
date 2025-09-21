65688471ce5baf78d208dadf1fc46b23e13d3e27
TaiNgo2112003 <Taingo2112003@gmail.com>
2025-09-21 12:40:35 +0700

    up

--- models/relationships.js
+++ models/relationships.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const friendshipSchema = new mongoose.Schema({
-  requester: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   recipient: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   status: {
     type: String,
